cc2ee09aae890ab4c683dd1116d28824c34a18c9
Valerian Backeberg <valerian@backebergs.de>
2019-09-23 21:29:33 +0200

    initial

---
+++ app/src/main/java/com/example/myapplication/Rezeption.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_rezeption.*
+
+class Rezeption : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rezeption)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
