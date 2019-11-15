9ea42c89685b5f30c5f8a84a349c73422037d3f5
Valerian Backeberg <vbackeberg@tum.de>
2019-11-15 20:27:05 +0100

    In Abteilungen view moodeel, remove repository. Join dao initialization.

--- app/src/main/java/com/example/myapplication/viewmodels/AbteilungenVM.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/AbteilungenVM.kt
@@ -3,21 +3,14 @@ package com.example.myapplication.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.data.daos.AbteilungDao
 import com.example.myapplication.data.databases.AbteilungenDatabase
 import com.example.myapplication.entities.Abteilung
 
 class AbteilungenVM(application: Application) : AndroidViewModel(application) {
-//    private val abteilungenRepository: AbteilungenRepository
-    private val abteilungDao: AbteilungDao
-    private val abteilungen: MutableLiveData<List<Abteilung>> = MutableLiveData()
-
-    init {
-        abteilungDao = AbteilungenDatabase
-            .getDatabase(application)
-            .abteilungDao()
-    }
+    private val abteilungDao: AbteilungDao = AbteilungenDatabase
+        .getDatabase(application)
+        .abteilungDao()
 
     fun getAbteilungen(): LiveData<List<Abteilung>> {
         return abteilungDao.findAll()
@@ -26,13 +19,4 @@ class AbteilungenVM(application: Application) : AndroidViewModel(application) {
     fun newAbteilung(abteilung: Abteilung): Long {
         return abteilungDao.insert(abteilung)
     }
-
-//    private fun loadAbteilungen() {
-//        try {
-//            abteilungen.value = abteilungenRepository.findAll()
-//            Log.d("abteilungenvm load abteilungen", abteilungen.value.toString())
-//        } catch (e: Exception) {
-//            Log.d("abteilungenvm load abteilungen", "failure")
-//        }
-//    }
 }
