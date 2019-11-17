6590c235d6c46638221e91b0490cb4d208b5c4dc
Valerian Backeberg <vbackeberg@tum.de>
2019-11-17 20:26:07 +0100

    Remove unused function from applicants.

--- app/src/main/java/com/example/myapplication/activities/Applicants.kt
+++ app/src/main/java/com/example/myapplication/activities/Applicants.kt
@@ -1,19 +1,12 @@
 package com.example.myapplication.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_applicants.*
 
 class Applicants : AppCompatActivity() {
-
-    fun kompetenz(@Suppress("UNUSED_PARAMETER") view: View) {
-        val intent = Intent(this, Kompetenz::class.java)
-        startActivity(intent)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
