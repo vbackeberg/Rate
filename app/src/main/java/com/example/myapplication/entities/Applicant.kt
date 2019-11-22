30fb5bbcb9bccfa137576cdca8137ee99ef13801
Valerian Backeberg <vbackeberg@tum.de>
2019-11-22 22:49:01 +0100

    Remove relation from applicant.

--- app/src/main/java/com/example/myapplication/entities/Applicant.kt
+++ app/src/main/java/com/example/myapplication/entities/Applicant.kt
@@ -2,12 +2,10 @@ package com.example.myapplication.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.Relation
 
 @Entity
 data class Applicant(
     @PrimaryKey(autoGenerate = true) val id: Long,
-    @Relation(parentColumn = "id", entityColumn = "applicantId") val competencies: List<Competency>,
     val positionId: Long,
     val abteilungId: Long
 )
