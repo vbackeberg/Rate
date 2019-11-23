598c86d889bd0cdbb0f468a53d2a85d595740956
Valerian Backeberg <vbackeberg@tum.de>
2019-11-23 16:36:38 +0100

    Implement applicants diff util callback

---
+++ app/src/main/java/com/example/myapplication/viewadapters/ApplicantsDiffUtilCallback.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.viewadapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.myapplication.entities.Applicant
+
+class ApplicantsDiffUtilCallback(
+    private val old: List<Applicant>,
+    private val new: List<Applicant>
+
+) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return old[oldItemPosition].id == new[newItemPosition].id
+    }
+
+    override fun getOldListSize(): Int = old.size
+
+    override fun getNewListSize(): Int = new.size
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return old[oldItemPosition].contentEquals(new[newItemPosition])
+    }
+}
