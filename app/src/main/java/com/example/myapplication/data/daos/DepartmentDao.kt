c36d9af498b9cf12256b3264d776777d96fc3e57
Valerian Backeberg <vbackeberg@tum.de>
2019-11-23 16:36:09 +0100

    Shorten import in department dao

--- app/src/main/java/com/example/myapplication/data/daos/DepartmentDao.kt
+++ app/src/main/java/com/example/myapplication/data/daos/DepartmentDao.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.data.daos
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.example.myapplication.entities.Department
 
@@ -13,6 +13,6 @@ interface DepartmentDao {
     @Query("SELECT * FROM department")
     fun findAll(): LiveData<List<Department>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = REPLACE)
     fun insert(department: Department): Long
 }
