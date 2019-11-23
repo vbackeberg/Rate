d887a9ebfba0424e10698e6895808e4eeee649c0
Valerian Backeberg <vbackeberg@tum.de>
2019-11-23 15:13:16 +0100

    Fix typo aNew new

--- app/src/main/java/com/example/myapplication/viewadapters/DepartmentsDiffUtilCallback.kt
+++ app/src/main/java/com/example/myapplication/viewadapters/DepartmentsDiffUtilCallback.kt
@@ -5,18 +5,18 @@ import com.example.myapplication.entities.Department
 
 class DepartmentsDiffUtilCallback(
     private val old: List<Department>,
-    private val aNew: List<Department>
+    private val new: List<Department>
 
 ) : DiffUtil.Callback() {
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return old[oldItemPosition].id == aNew[newItemPosition].id
+        return old[oldItemPosition].id == new[newItemPosition].id
     }
 
     override fun getOldListSize(): Int = old.size
 
-    override fun getNewListSize(): Int = aNew.size
+    override fun getNewListSize(): Int = new.size
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return old[oldItemPosition].name == aNew[newItemPosition].name
+        return old[oldItemPosition].name == new[newItemPosition].name
     }
 }
