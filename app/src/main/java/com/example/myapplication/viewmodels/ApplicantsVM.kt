973b40acd9349aa9ca5ec2d18e236cd9bb8d5506
Valerian Backeberg <vbackeberg@tum.de>
2019-11-27 21:55:56 +0100

    In applicants vm use current department and position id when creating new applicant.

--- app/src/main/java/com/example/myapplication/viewmodels/ApplicantsVM.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/ApplicantsVM.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication.viewmodels
 
 import android.app.Application
+import android.content.Context.MODE_PRIVATE
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import com.example.myapplication.CURRENT_DEPARTMENT_ID
+import com.example.myapplication.CURRENT_POSITION_ID
 import com.example.myapplication.entities.Applicant
 import com.example.myapplication.repositories.ApplicantsRepository
 import kotlinx.coroutines.CoroutineScope
@@ -12,8 +15,16 @@ import kotlinx.coroutines.launch
 class ApplicantsVM(application: Application) : AndroidViewModel(application) {
     private val applicantsRepository = ApplicantsRepository(application)
 
+    private val departmentId = getApplication<Application>()
+    .getSharedPreferences(CURRENT_DEPARTMENT_ID, MODE_PRIVATE)
+    .getLong(CURRENT_DEPARTMENT_ID, 0L)
+
+    private val positionId = getApplication<Application>()
+    .getSharedPreferences(CURRENT_POSITION_ID, MODE_PRIVATE)
+    .getLong(CURRENT_POSITION_ID, 0L)
+
     fun getAll(): LiveData<List<Applicant>> {
-        return applicantsRepository.findAllByPositionAndDepartment(1L, 1L)
+        return applicantsRepository.findAllByPositionAndDepartment(positionId, departmentId)
     }
 
     fun new(applicant: Applicant) = CoroutineScope(Dispatchers.IO).launch {
