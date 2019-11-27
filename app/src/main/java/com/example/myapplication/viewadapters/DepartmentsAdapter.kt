4bd57f6b205c956d0fb864cbbf20fc232baeb767
Valerian Backeberg <vbackeberg@tum.de>
2019-11-27 21:34:58 +0100

    Set current department id from view holder.

--- app/src/main/java/com/example/myapplication/viewadapters/DepartmentsAdapter.kt
+++ app/src/main/java/com/example/myapplication/viewadapters/DepartmentsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.viewadapters
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.CURRENT_DEPARTMENT_ID
 import com.example.myapplication.R
-import com.example.myapplication.activities.Competencies
+import com.example.myapplication.activities.Applicants
 import com.example.myapplication.entities.Department
 import kotlinx.android.synthetic.main.item_departments.view.*
 
@@ -39,15 +41,22 @@ class DepartmentsAdapter : RecyclerView.Adapter<DepartmentsAdapter.DepartmentVie
 
     class DepartmentViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         private val name: TextView = view.departmentName
+        private var id = 0L
 
         init {
             view.setOnClickListener {
-                view.context.startActivity(Intent(view.context, Competencies::class.java))
+                view.context.startActivity(Intent(view.context, Applicants::class.java))
+
+                view.context
+                    .getSharedPreferences(CURRENT_DEPARTMENT_ID, MODE_PRIVATE)
+                    .edit().putLong(CURRENT_DEPARTMENT_ID, id)
+                    .apply()
             }
         }
 
         fun bind(department: Department) {
             name.text = department.name
+            id = department.id
         }
     }
 
