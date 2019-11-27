3004c0fe829c22b7b01407a9da9a6f91c108aa99
Valerian Backeberg <vbackeberg@tum.de>
2019-11-27 21:59:04 +0100

    Create evaluation activity and additional item layout

---
+++ app/src/main/java/com/example/myapplication/activities/Evaluation.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.activities
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+import kotlinx.android.synthetic.main.activity_evaluation.*
+
+class Evaluation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evaluation)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
