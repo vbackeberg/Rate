96b8f50be4ee031caad645fee8ac3172ecb65cde
Valerian Backeberg <vbackeberg@tum.de>
2019-11-27 21:37:13 +0100

    Add Current Department Id constant.

--- app/src/main/java/com/example/myapplication/Constants.kt
+++ app/src/main/java/com/example/myapplication/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.myapplication
 
 const val CURRENT_APPLICANT_ID = "current_applicant_id"
+const val CURRENT_DEPARTMENT_ID = "current_department_id"
 const val CURRENT_POSITION_ID = "current_position_id"
