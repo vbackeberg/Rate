810ced0d21992d0d6f0064f5deb7c4e0fb1dabe5
Valerian Backeberg <valerian@backebergs.de>
2019-12-08 21:28:50 +0100

    In score service, update competencies using importance from competency area.

--- app/src/main/java/com/example/myapplication/services/ScoreService.kt
+++ app/src/main/java/com/example/myapplication/services/ScoreService.kt
@@ -1,27 +1,43 @@
 package com.example.myapplication.services
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.CURRENT_APPLICANT_ID
 import com.example.myapplication.entities.Competency
 import com.example.myapplication.repositories.ApplicantsRepository
 import com.example.myapplication.repositories.CompetenciesRepository
+import com.example.myapplication.repositories.CompetencyAreasRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 class ScoreService(application: Application) {
     private val applicantsRepository = ApplicantsRepository.getInstance(application)
-
-    private var applicantId = application
-        .getSharedPreferences(CURRENT_APPLICANT_ID, AppCompatActivity.MODE_PRIVATE)
-        .getLong(CURRENT_APPLICANT_ID, 0L)
-
     private var competenciesRepository = CompetenciesRepository(application)
+    private var competencyAreasRepository = CompetencyAreasRepository.getInstance(application)
 
-    suspend fun update(competency: Competency) {
-        val competenciesNew = competenciesRepository.findAllByApplicantSuspend(applicantId)
-        applicantsRepository.updateScore(competency.applicantId, calculateScore(competenciesNew))
+    suspend fun update(applicantId: Long, positionId: Long) {
+        val importanceByCompetencyArea = CoroutineScope(Dispatchers.IO)
+            .async {
+                competencyAreasRepository
+                    .findAllByPositionSuspend(positionId)
+                    .associate { Pair(it.id, it.importance) }
+            }
+
+        val competenciesNew = CoroutineScope(Dispatchers.IO)
+            .async { competenciesRepository.findAllByApplicantSuspend(applicantId) }
+
+        applicantsRepository.updateScore(
+            applicantId,
+            calculateScore(competenciesNew.await(), importanceByCompetencyArea.await())
+        )
     }
 
-    private fun calculateScore(competencies: List<Competency>): Int {
-        return competencies.sumBy { competency -> competency.value * competency.importance }
+    private fun calculateScore(
+        competencies: List<Competency>,
+        importanceByCompetencyArea: Map<Long, Int>
+    ): Int {
+
+        return competencies.sumBy { competency ->
+            competency.value * importanceByCompetencyArea.getValue(competency.competencyAreaId)
+        }
     }
 }
