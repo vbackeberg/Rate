ba17467f66cae6d56dd6b70254afcb184fea1dc2
Valerian Backeberg <valerian@backebergs.de>
2019-12-13 18:16:01 +0100

    In competency area vm use repository instead of dao. Add update and insert to competency dao.

--- app/src/main/java/com/example/myapplication/repositories/CompetencyAreasRepository.kt
+++ app/src/main/java/com/example/myapplication/repositories/CompetencyAreasRepository.kt
@@ -1,19 +1,33 @@
 package com.example.myapplication.repositories
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import com.example.myapplication.SingletonHolder
 import com.example.myapplication.data.daos.CompetencyAreaDao
 import com.example.myapplication.data.databases.CompetencyAreasDatabase
 import com.example.myapplication.entities.CompetencyArea
 
-class CompetencyAreasRepository private  constructor(application: Application){
-private val competencyAreaDao: CompetencyAreaDao = CompetencyAreasDatabase
-    .getDatabase(application)
-    .competencyAreaDao()
+class CompetencyAreasRepository private constructor(application: Application) {
+    private val competencyAreaDao: CompetencyAreaDao = CompetencyAreasDatabase
+        .getDatabase(application)
+        .competencyAreaDao()
+
+    fun findAllByPosition(positionId: Long): LiveData<List<CompetencyArea>> {
+        return competencyAreaDao.findAllByPosition(positionId)
+    }
 
     suspend fun findAllByPositionSuspend(positionId: Long): List<CompetencyArea> {
         return competencyAreaDao.findAllByPositionSuspend(positionId)
     }
 
-    companion object : SingletonHolder<CompetencyAreasRepository, Application>(::CompetencyAreasRepository)
+    suspend fun insert(competencyArea: CompetencyArea) {
+        return competencyAreaDao.insert(competencyArea)
+    }
+
+    suspend fun update(competencyArea: CompetencyArea) {
+        return competencyAreaDao.update(competencyArea)
+    }
+
+    companion object :
+        SingletonHolder<CompetencyAreasRepository, Application>(::CompetencyAreasRepository)
 }
--- app/src/main/java/com/example/myapplication/viewmodels/CompetencyAreasVM.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/CompetencyAreasVM.kt
@@ -3,23 +3,24 @@ package com.example.myapplication.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import com.example.myapplication.data.daos.CompetencyAreaDao
-import com.example.myapplication.data.databases.CompetencyAreasDatabase
 import com.example.myapplication.entities.CompetencyArea
+import com.example.myapplication.repositories.CompetencyAreasRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class CompetencyAreasVM(application: Application) : AndroidViewModel(application) {
-    private val competencyAreaDao: CompetencyAreaDao = CompetencyAreasDatabase
-        .getDatabase(application)
-        .competencyAreaDao()
+    private val competencyAreasRepository = CompetencyAreasRepository.getInstance(application)
 
     fun getAll(positionId: Long): LiveData<List<CompetencyArea>> {
-        return competencyAreaDao.findAllByPosition(positionId)
+        return competencyAreasRepository.findAllByPosition(positionId)
     }
 
     fun new(competencyArea: CompetencyArea) = CoroutineScope(Dispatchers.IO).launch {
-        competencyAreaDao.insert(competencyArea)
+        competencyAreasRepository.insert(competencyArea)
+    }
+
+    fun update(competencyArea: CompetencyArea) = CoroutineScope(Dispatchers.IO).launch {
+        competencyAreasRepository.update(competencyArea)
     }
 }
